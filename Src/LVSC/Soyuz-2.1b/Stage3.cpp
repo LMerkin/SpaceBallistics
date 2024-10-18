be1c50c6caa7ecbcedb3cd3b43b519a62fe52d90
Prof Dr Leonid Merkin-Janson <leonid.a.merkin@gmail.com>
2024-10-18 12:19:00 +0300

    [+] SpaceBallistics: All actual code moved to Headers; Src/*.cpp files only #include them

--- Src/LVSC/Soyuz-2.1b/Stage3.cpp
+++ Src/LVSC/Soyuz-2.1b/Stage3.cpp
@@ -1,6 +1,6 @@
 // vim:ts=2:et
 //===========================================================================//
-//                     "Src/LVSC/Soyuz-2.1b/Stage3.cpp":                     //
+//                    "Src/LVSC/Soyuz-2.1b/Stage3.cpp":                      //
 //                         Actual Code Generation                            //
 //===========================================================================//
 #include "SpaceBallistics/LVSC/Soyuz-2.1b/Stage3.hpp"
