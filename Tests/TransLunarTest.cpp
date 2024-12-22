2d810d6e4f14257c8becd71ea6f97f4621bd9c95
Prof Dr Leonid Merkin-Janson <leonid.a.merkin@gmail.com>
2024-12-22 18:11:02 +0300

    [+-] TransLunarTest completed

--- Tests/TransLunarTest.cpp
+++ Tests/TransLunarTest.cpp
@@ -26,12 +26,12 @@ namespace
   struct Context
   {
     Time                    m_mjs;    // TimeStamp (MJS_TDB)
-    PosKV_GCRS<>            m_pos;    // SC Pos    (DE440T)
-    VelKV_GCRS<>            m_vel;    // SC Vel    (DE440T)
-    PosKV_GCRS<Body::Moon>  m_posM;   // Moon Pos  (DE440T)
-    VelKV_GCRS<Body::Moon>  m_velM;   // Moon Vel  (DE440T)
-    LenK                    m_rho;    // SC Distance from Earth
-    LenK                    m_rhoM;   // SC Distance from the Moon (DE440T)
+    PosKV_GCRS<>            m_posEK;  // SC Pos    (DE440T)
+    VelKV_GCRS<>            m_velEK;  // SC Vel    (DE440T)
+    PosKV_GCRS<Body::Moon>  m_posEM;  // Moon Pos  (DE440T)
+    VelKV_GCRS<Body::Moon>  m_velEM;  // Moon Vel  (DE440T)
+    LenK                    m_rhoEK;  // SC Distance from Earth
+    LenK                    m_rhoMK;  // SC Distance from the Moon (DE440T)
   };
 
   //=========================================================================//
@@ -47,29 +47,29 @@ namespace
     // which occurred not later than "a_t0":
     TDB   a_t0,
 
-    // "a_fQ": Mean Anomaly of the Moon which corresponds to the ApoGee of the
+    // "a_fM": Mean Anomaly of the Moon which corresponds to the apogee of the
     // TLO; the Lunar "rendez-vois" of the SC will occur near that point. From
-    // the Delta-V considerations, it would be advantageous to have "a_fQ" near
+    // the Delta-V considerations, it would be advantageous to have "a_fM" near
     // 0 (near the Moon Orbit perigee), but it may not always be possible  due
     // to various launch constraints:
     //
-    Angle a_fQ     = 0.0_rad,
+    Angle a_fM,
 
-    // "a_deltaQ" is the excess ApoGee distance of the LRO over the Moon Orbit
-    // Radius-Vector at "a_fQ"; eg for SelenoCentric Orbit (SCO) Insertion, it
+    // "a_deltaQ" is the excess apogee distance of the LRO over the Moon Orbit
+    // Radius-Vector at "a_fM"; eg for SelenoCentric Orbit (SCO) Insertion, it
     // should be greater than the Moon Radius:
     //
-    LenK  a_deltaQ = 2000.0_km,
+    LenK  a_deltaQ,
 
     // For SCO insertion, provide some estimated separation from the leading
     // edge of the Moon:
-    LenK  a_edge   = 300.0_km,
+    LenK  a_edge,
 
     // TLO insertion is assumed to be from a circular LEO. TODO: Provide full
     // LEO params here. For the moment, we just provide the curcular altitude
     // relative to the Earth Equator:
     //
-    LenK  a_leoH   = 200.0_km
+    LenK  a_leoH
   )
   {
     //-----------------------------------------------------------------------//
@@ -88,16 +88,16 @@ namespace
     Angle  I     = moonOrbit.I();
     Angle  Omega = moonOrbit.Omega();
 
-    // Longitude of the TLO ApoGee (from the common Ascending Node of the Lunar
+    // Longitude of the TLO apogee (from the common Ascending Node of the Lunar
     // Orbit and the TLO):
-    Angle  lQ    = moonOrbit.omega() + a_fQ;
+    Angle  lQ    = moonOrbit.omega() + a_fM;
 
     // Then the Longitude of the TLO perigee from the Node is actually the Arg-
     // ument of the perigee:
     Angle  omega = lQ - PI;
 
-    // Lunar Radius-Vector at "a_fQ" and the TLO ApoGee:
-    LenK   Q     = moonOrbit.GetR(a_fQ) + a_deltaQ;
+    // Moon Radius-Vector @ "a_fM", and the TLO apogee:
+    LenK   Q     = moonOrbit.GetR(a_fM) + a_deltaQ;
 
     // XXX: For the moment, we assume that the TLO inserion point is the TLO
     // perigee, corresponding to the LEO radius. Then:
@@ -139,12 +139,6 @@ namespace
     // "fRM": The True Anomaly of the Moon at "lR":
     Angle fRM =  lR - omegaM;
 
-    // The corresp distance (must be same for Moon and SC):
-    LenK  rRM =  moonOrbit.GetR(fRM);
-
-    // Adjust "fRM" by the angle corresponding to "a_edge" + Moon Radius:
-    fRM -= Angle(double((BodyData<Body::Moon>::Re + a_edge) / rRM));
-
     // Next time (after the previous Moon perigee) the Moon will be at that fRM:
     Time tauM = moonOrbit.TimeSincePeriFocus(fRM);
     TDB  tRM  = moonOrbit.T() + tauM;
@@ -152,9 +146,19 @@ namespace
     // The corresp True Anomaly of the TLO:
     Angle fR  = lR - omega;
 
-    // The TLO flight time from the perigee (XXX: it may change!) to the "fR":
+    // The TLO flight time from the perigee to the "fR":
     Time tau  = tlo0.TimeSincePeriFocus(fR);
     TDB  T    = tRM - tau;
+
+    // Now, start a bit earlier (and arrive at the orbit intersection point
+    // earlier) in order to avoid direct collision with the Moon and be able
+    // to perform the SCO insertion maneuver.
+    // The approx time required by the Moon to cover our "edge" (with the Moon
+    // radius added):
+    //
+    a_edge      += BodyData<Body::Moon>::Re;
+    Time advTime = a_edge / VelK(velM);
+    T           -= advTime;
 
     // Finally, the full TLO Elements:
     return TwoBodyOrbit<GCRS>::MkEllipticOrbit(a, e, I, Omega, omega, T);
@@ -238,71 +242,194 @@ namespace
     a_y_dot[4] = acc.y().Magnitude();
     a_y_dot[5] = acc.z().Magnitude();
 
-    // Memoise some intermediate data in "ctx":
+    // Memoise some intermediate data in the Context:
     Context* ctx = reinterpret_cast<Context*>(a_ctx);
     ctx->m_mjs   = mjs_tdb;
-    ctx->m_pos   = posEK;
-    ctx->m_vel   = velEK;
-    ctx->m_posM  = posEM;
-    ctx->m_velM  = velEM;
-    ctx->m_rho   = rhoEK;
-    ctx->m_rhoM  = rhoMK;
+    ctx->m_posEK = posEK;
+    ctx->m_velEK = velEK;
+    ctx->m_posEM = posEM;
+    ctx->m_velEM = velEM;
+    ctx->m_rhoEK = rhoEK;
+    ctx->m_rhoMK = rhoMK;
 
     // All Done!
     return 0;
   }
-}
+
+  //=========================================================================//
+  // "PropagateTLO":                                                         //
+  //=========================================================================//
+  struct TLORes
+  {
+    LenK  m_deltaQ;       // Excess TLO apogee distance
+    LenK  m_edge;         // TLO "aiming edge"
+    LenK  m_minDist;      // Minimum Distance to the Moon Surface
+    TDB   m_minDistTime;  // Time of the Closest Approach
+    VelK  m_totalV;       // See the impl...
+  };  
+
+  TLORes PropagateTLO
+  (
+    TDB      a_t0,        // Ref Time
+    Angle    a_fM,        // "Aim Point": True Anomaly of the Moon Orbit
+    LenK     a_deltaQ,    // Excess TLO apogee distance
+    LenK     a_edge,      // TLO "aiming edge"
+    LenK     a_leoH,      // Circular LEO altitude to start from
+    gsl_odeiv2_driver* a_ODEDriver,
+    Time     a_tauObs,    // Observation Time Step
+    Context* a_ctx,
+    bool     a_verbose
+  )
+  {
+    assert(a_ODEDriver != nullptr && a_ctx != nullptr);
+
+    //-----------------------------------------------------------------------//
+    // Construct the TLO:                                                    //
+    //-----------------------------------------------------------------------//
+    // XXX: Once again, it is currently assumed that the TLO insertion occurs
+    // at TLO the perigee. We assume that we are fying into the Moon Orbit pe-
+    // rigee as well:
+    TwoBodyOrbit<GCRS> tlo = MkTLO(a_t0, a_fM, a_deltaQ, a_edge, a_leoH);
+
+    if (a_verbose)
+      printf("TLO:\n\ta=%.3lf\n\tP=%.3lf\n\te=%.9lf\n\tQ=%.3lf\n\tI=%.6lf\n"
+             "\tOmega=%.6lf\n\tomega=%.6lf\n\tM0=%.6lf\n\tT=%.3lf\n",
+             tlo.a()                    .Magnitude(),
+             tlo.P()                    .Magnitude(),
+             tlo.e(),
+             tlo.Q()                    .Magnitude(),
+             To_Angle_deg(tlo.I())      .Magnitude(),
+             To_Angle_deg(tlo.Omega())  .Magnitude(),
+             To_Angle_deg(tlo.omega())  .Magnitude(),
+             To_Angle_deg(tlo.M0())     .Magnitude(),
+             tlo.T().GetTimeSinceEpoch().Magnitude());
+
+    // Get the initial Pos and Vel vectors for the TLO (at the TLO perigee,
+    // hence f=0 here):
+    PosKV_GCRS<> pos0;
+    VelKV_GCRS<> vel0;
+    tlo.GetPV(0.0_rad, &pos0, &vel0);
+
+    if (a_verbose)
+      printf("TLO Insertion: R=%.3lf, V=%.3lf\n",
+             LenK(pos0).Magnitude(), VelK(vel0).Magnitude());
+
+    //-----------------------------------------------------------------------//
+    // System Definition for GSL-based direct TLO integration:               //
+    //-----------------------------------------------------------------------//
+    // "UnTyped" initial state vector for GSL:
+    double y[ODEDim]
+    {
+      pos0.x().Magnitude(), pos0.y().Magnitude(), pos0.z().Magnitude(),
+      vel0.x().Magnitude(), vel0.y().Magnitude(), vel0.z().Magnitude()
+    };
+
+    //-----------------------------------------------------------------------//
+    // Run the Time-Marshalling:                                             //
+    //-----------------------------------------------------------------------//
+    // Time Range of Integration: 10d are enough (from the TLO insertion @ the
+    // TLO perigee):
+    TDB const tFrom = tlo.T();
+    TDB const tTo   = tFrom + To_Time(10.0_day);
+
+    // Min distance to the Moon center:
+    LenK        minRhoM    = LenK(+Inf<double>);
+    TDB         minRhoMTime;
+    VelKV_GCRS  minRhoMVel;  // Relative velocity at the closest approach
+
+    TDB t = tFrom;
+    while (t < tTo)
+    {
+      //---------------------------------------------------------------------//
+      // Make the Step:                                                      //
+      //---------------------------------------------------------------------//
+      double mjs  = t.GetTimeSinceEpoch().Magnitude();
+      double mjs1 = mjs + a_tauObs.Magnitude();
+
+      int rc = gsl_odeiv2_driver_apply(a_ODEDriver, &mjs, mjs1, y);
+
+      if (UNLIKELY(rc != 0))
+      {
+        cout << "# ERROR: RC=" << rc << ", exiting..." << endl;
+        break;
+      }
+      // We should have arrived at "mjs1"?
+      assert(mjs == mjs1);
+
+      // Update "t" for the next iteration:
+      t = TDB(Time(mjs));
+
+      //---------------------------------------------------------------------//
+      // Step Processing:                                                    //
+      //---------------------------------------------------------------------//
+      if (a_verbose)
+        printf("%.3lf\t%.3lf\t%.3lf\n",
+               a_ctx->m_mjs  .Magnitude(), a_ctx->m_rhoEK.Magnitude(),
+               a_ctx->m_rhoMK.Magnitude());
+
+      // Minimum separation detection:
+      if (a_ctx->m_rhoMK < minRhoM)
+      {
+        minRhoM     = a_ctx->m_rhoMK;
+        minRhoMTime = t;
+        // The SC velocity relative to the Moon at that time:
+        minRhoMVel  = a_ctx->m_velEK - a_ctx->m_velEM;
+      }
+    }
+    //-----------------------------------------------------------------------//
+    // The Result:                                                           //
+    //-----------------------------------------------------------------------//
+    // We need to estimate the Total DeltaV which is made of:
+    // (1) the velocity of the TLO perigee (irrespective to how we get there,
+    //     eg from the LEO);
+    // (2) the velocity relative to the Moon at the closest approach point;
+    // (3) minus the circular velocity at that distance from the Moon:
+    //
+    VelK circV =
+      SqRt(DE440T::K<Body::Moon> / (BodyData<Body::Moon>::Re + minRhoM));
  
+    TLORes res
+    {
+      .m_deltaQ      = a_deltaQ,
+      .m_edge        = a_edge,
+      .m_minDist     = minRhoM - BodyData<Body::Moon>::Re,
+      .m_minDistTime = minRhoMTime,
+      .m_totalV      = VelK(vel0) + VelK(minRhoMVel) - circV
+    };
+    return res;
+  }
+}
+
 //===========================================================================//
 // "main":                                                                   //
 //===========================================================================//
 int main()
 {
-  TDB t0{TT{UTC{{2025, 1, 1}}}};
-
-  //------------------------------------------------------------------------//
-  // Construct the TLO:                                                     //
-  //------------------------------------------------------------------------//
-  // XXX: Once again, it is currently assumed that the TLO
-  // insertion occurs at the perigee; the perigee time is in general NOT "t0"!
-  //
-  TwoBodyOrbit<GCRS> tlo = MkTLO(t0);
-
-  printf("TLO:\n\ta=%.3lf\n\tP=%.3lf\n\te=%.9lf\n\tQ=%.3lf\n\tI=%.6lf\n"
-         "\tOmega=%.6lf\n\tomega=%.6lf\n\tM0=%.6lf\n\tT=%.3lf\n",
-         tlo.a()                    .Magnitude(),
-         tlo.P()                    .Magnitude(),
-         tlo.e(),
-         tlo.Q()                    .Magnitude(),
-         To_Angle_deg(tlo.I())      .Magnitude(),
-         To_Angle_deg(tlo.Omega())  .Magnitude(),
-         To_Angle_deg(tlo.omega())  .Magnitude(),
-         To_Angle_deg(tlo.M0())     .Magnitude(),
-         tlo.T().GetTimeSinceEpoch().Magnitude());
-
-  // Get the initial Pos and Vel vectors for the TLO (at the perigee, hence
-  // f=0):
-  PosKV_GCRS<> pos0;
-  VelKV_GCRS<> vel0;
-  tlo.GetPV(0.0_rad, &pos0, &vel0);
-
-  printf("TLO Insertion: R=%.3lf, V=%.3lf\n",
-         LenK(pos0).Magnitude(), VelK(vel0).Magnitude());
+  //-------------------------------------------------------------------------//
+  // Consts:                                                                 //
+  //-------------------------------------------------------------------------//
+  TDB const       t0        {TT{UTC{{2025, 1, 1}}}};
+  constexpr Angle fM      = 0.0_rad;
+  constexpr LenK  leoH    = 200.0_km;
+  bool            verbose = false;
 
   //-------------------------------------------------------------------------//
-  // System Definition for GSL-based direct TLO integration:                 //
+  // Setup:                                                                  //
   //-------------------------------------------------------------------------//
+  // GSL ODE Solver Initialisation:
   // Presumably, for an explicit integration method, no Jacobian of the RHS is
   // required:
   Context ctx;
   gsl_odeiv2_system ODE { ODERHS, nullptr, ODEDim, &ctx };
 
-  // "UnTyped" initial state vector for GSL:
-  double y[ODEDim]
-  {
-    pos0.x().Magnitude(), pos0.y().Magnitude(), pos0.z().Magnitude(),
-    vel0.x().Magnitude(), vel0.y().Magnitude(), vel0.z().Magnitude()
-  };
+  // Ranges for "deltaQ" and "edge":
+  constexpr LenK deltaQ_from =  5000.0_km;
+  constexpr LenK deltaQ_to   = 30000.0_km;
+  constexpr LenK deltaQ_step =   250.0_km;
+
+  constexpr LenK edge_from   =  2000.0_km;
+  constexpr LenK edge_to     = 15000.0_km;
+  constexpr LenK edge_step   =   250.0_km;
 
   // Construct the RKF45 Integrator with 10 sec initial TimeStep (used by the
   // Driver internally):
@@ -321,33 +448,24 @@ int main()
        tau.Magnitude(), AbsPrec.Magnitude(), RelPrec);
   assert(ODEDriver != nullptr);
 
-  // Time Range of Integration: 10d are enough (from the TLO insertion at the
-  // perigee):
-  TDB const tFrom = tlo.T();
-  TDB const tTo   = tFrom + To_Time(10.0_day);
-
   //-------------------------------------------------------------------------//
-  // Run the Time-Marshalling:                                               //
+  // Iterate over the "deltaQ" and "edge":                                   //
   //-------------------------------------------------------------------------//
-  double       mjs   = tFrom.GetTimeSinceEpoch().Magnitude();
-  double const mjsTo = tTo  .GetTimeSinceEpoch().Magnitude();
-
-  while (mjs < mjsTo)
+  for (LenK deltaQ = deltaQ_from; deltaQ <= deltaQ_to; deltaQ += deltaQ_step)
+  for (LenK edge   = edge_from;   edge   <= edge_to;   edge   += edge_step)
   {
-    double mjs1 = mjs + tauObs.Magnitude();
+    TLORes res =
+      PropagateTLO(t0,    fM, deltaQ, edge, leoH,
+                   ODEDriver, tauObs, &ctx, verbose);
 
+    printf("%.3lf\t%.3lf\t%.3lf\t%.3lf\n",
+           deltaQ.Magnitude(), edge.Magnitude(), res.m_minDist.Magnitude(),
+           res.m_totalV.Magnitude());
 
-    int rc = gsl_odeiv2_driver_apply(ODEDriver, &mjs, mjs1, y);
-
-// FIXME:
-printf("%.3lf\t%.3lf\t%.3lf\n", ctx.m_mjs.Magnitude(), ctx.m_rho.Magnitude(), ctx.m_rhoM.Magnitude());
-
-    if (UNLIKELY(rc != 0))
-    {
-      cout << "# ERROR: RC=" << rc << ", exiting..." << endl;
-      break;
-    }
+    // Reset the GSL Driver before the next iteration:
+    gsl_odeiv2_driver_reset(ODEDriver);
   }
+
   // De-Allocate the Driver:
   (void) gsl_odeiv2_driver_free(ODEDriver);
 
