19e7d2bbc95e725ca7cefb7955eee5d237358e59
Prof Dr Leonid Merkin-Janson <leonid.a.merkin@gmail.com>
2025-02-24 10:49:04 +0300

    [+-] SpaceBallistics: Adjustments to the SU2 setup (compressible, inviscid) for the Thin Elliptic Wing

--- SU2/Scripts/MkAllEWCases.sh
+++ SU2/Scripts/MkAllEWCases.sh
@@ -7,18 +7,20 @@ TopDir=$(dirname $AbsPath0)
 cd $TopDir/../EllipticWing
 
 # Clean everything up:
-ewDir=~/Tmp/EllipticWing
+ewDir=/var/tmp/EllipticWing
 rm -fr $ewDir
 mkdir  $ewDir
 
-# Generate all cases under "$ewDir":
+#-----------------------------------------------------------------------------#
+# Generate All Cases under "$ewDir":                                          #
+#-----------------------------------------------------------------------------#
 # Angle of Attack, in deg:
-for alphaDeg in {5..15..1}
+for alphaDeg in {0..15..1}
 do
   # Mach Number at infinity (as a multiple 0.01):
-  for M100 in {30..300..5}
+  for M100 in {10..300..5}
   do
-		M=$(echo $M100 | awk '{printf("%.02f", $1/100)}')
+    M=$(echo $M100 | awk '{printf("%.02f", $1/100)}')
 
     # Create the CaseDir:
     caseDir=$(printf $ewDir/%02d/%s $alphaDeg $M)
@@ -26,10 +28,30 @@ do
 
     # Copy the Config.cfg over and install the variable params in it:
     cp Config.cfg $caseDir
-		echo -e "MACH_NUMBER= $M\nAOA= $alphaDeg\n" >> $caseDir/Config.cfg
+    echo -e "MACH_NUMBER= $M\nAOA= $alphaDeg\n" >> $caseDir/Config.cfg
 
-		# There is no need to copy the Mesh file; symlink will do:
-		ln -s $PWD/Mesh.su2 $caseDir/Mesh.su2
+    # There is no need to copy the Mesh file; symlink will do:
+    ln -s $PWD/Mesh.su2 $caseDir/Mesh.su2
   done
+done
+
+#-----------------------------------------------------------------------------#
+# Generate the Makefile:                                                      #
+#-----------------------------------------------------------------------------#
+cd $ewDir
+shopt -s nullglob
+AllCases=(*/*)
+AllTargs=${AllCases[@]/%/\/Coeffs}
+
+echo "all: ${AllTargs[@]}" > Makefile
+for c  in  ${AllCases[@]}
+do
+  alpha=$(dirname $c)
+  M=$(basename $c)
+  echo -e \
+    "\n$c/Coeffs: $c/Config.cfg\n\tcd $c &&" \
+    "SU2_CFD Config.cfg >& cfd.log &&" \
+    "awk -v alpha=$alpha -v M=$M -f $TopDir/EWCoeffs.awk cfd.log > Coeffs" \
+    >> Makefile
 done
 # Generation Done!
--- SU2/Scripts/RunAllEWCases.sh
+++ SU2/Scripts/RunAllEWCases.sh
@@ -5,35 +5,14 @@
 # How many processes to run in parallel:
 NPar=$(( $(nproc) / 2 ))
 
-AbsPath0=$(realpath $0)
-TopDir=$(dirname $AbsPath0)
-
-shopt -s nullglob
-ewDir=~/Tmp/EllipticWing
+ewDir=/var/tmp/EllipticWing
 cd $ewDir
-
-#=============================================================================#
-AllCases=(*/*)
-#=============================================================================#
-AllTargs=${AllCases[@]/%/\/Coeffs}
-
-# Generate the Makefile:
-echo "all: ${AllTargs[@]}" > Makefile
-for c  in  ${AllCases[@]}
-do
-  alpha=$(dirname $c)
-  M=$(basename $c)
-  echo -e \
-    "\n$c/Coeffs: $c/Config.cfg\n\tcd $c &&" \
-    "SU2_CFD Config.cfg >& cfd.log &&" \
-    "awk -v alpha=$alpha -v M=$M -f $TopDir/EWCoeffs.awk cfd.log > Coeffs" \
-    >> Makefile
-done
 
 # Run "make" for all cases, creating at most $NPar parallel processes:
 make -j $NPar
 
 # Collect the Coeffs for each AngleOfAttack, for all M vals:
+shopt -s nullglob
 AllCoeffs=(*/*/Coeffs)
 [[ ${#AllCoeffs[@]} -eq 0 ]] || cat ${AllCoeffs[@]} > Coeffs
 
