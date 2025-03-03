5aa073d91d1ea0728b38b46cb0ae64e88d7eb2e8
Prof Dr Leonid Merkin-Janson <leonid.a.merkin@gmail.com>
2025-03-03 13:57:30 +0000

    [-+] SpaceBallistics: New SU2/EllipticWing configs and scripts, still in testing

--- SU2/Scripts/MkAllEWCases.sh
+++ SU2/Scripts/MkAllEWCases.sh
@@ -6,17 +6,34 @@ AbsPath0=$(realpath $0)
 TopDir=$(dirname $AbsPath0)
 cd $TopDir/../EllipticWing
 
+NProc=$(nproc)
+
+#-----------------------------------------------------------------------------#
+# Top Matter:																																	#
+#-----------------------------------------------------------------------------#
 # Clean everything up:
-ewDir=/var/tmp/EllipticWing
+ewDir=~/Tmp/EW
 rm -fr $ewDir
 mkdir  $ewDir
+
+# Generate the Mesh File:
+$TopDir/../../__BUILD__/GCC-Debug/bin/MkZhukovskyMesh \
+	-A 10 -N 256 -o $ewDir/MeshEW.su2
 
 #-----------------------------------------------------------------------------#
 # Generate All Cases under "$ewDir":                                          #
 #-----------------------------------------------------------------------------#
 # Angle of Attack, in deg:
-for alphaDeg in {0..15..1}
+for alphaDeg in {0..30..1}
 do
+	# NB: The Convergence Criterion Field depends on "alphaDeg":
+	if [ $alphaDeg -eq 0 ]
+	then
+		ConvFld=DRAG
+	else
+		ConvFld=LIFT
+	fi
+
   # Mach Number at infinity (as a multiple 0.01):
   for M100 in {10..300..5}
   do
@@ -26,12 +43,26 @@ do
     caseDir=$(printf $ewDir/%02d/%s $alphaDeg $M)
     mkdir -p $caseDir
 
-    # Copy the Config.cfg over and install the variable params in it:
-    cp Config.cfg $caseDir
-    echo -e "MACH_NUMBER= $M\nAOA= $alphaDeg\n" >> $caseDir/Config.cfg
+    # Copy the Config-Euler.cfg over:
+    cp Config-Euler.cfg $caseDir/C.cfg
+
+    # Install the variable params.
+    # NB: The MaxCFL depends on the Mach Number:
+    if [ $M100 -le 30 -o $M100 -ge 170 ]
+		then
+			MaxCFL=1
+		else
+			MaxCFL=0.1
+		fi
+    cat >> $caseDir/C.cfg <<EOF
+MACH_NUMBER= $M
+AOA= $alphaDeg
+CFL_ADAPT_PARAM= (0.618, 1.618, 0.01, $MaxCFL, 1E-5)
+CONV_FIELD= $ConvFld
+EOF
 
     # There is no need to copy the Mesh file; symlink will do:
-    ln -s $PWD/Mesh.su2 $caseDir/Mesh.su2
+    ln -s $ewDir/MeshEW.su2 $caseDir/MeshEW.su2
   done
 done
 
@@ -49,8 +80,8 @@ do
   alpha=$(dirname $c)
   M=$(basename $c)
   echo -e \
-    "\n$c/Coeffs: $c/Config.cfg\n\tcd $c &&" \
-    "SU2_CFD Config.cfg >& cfd.log &&" \
+    "\n$c/Coeffs: $c/C.cfg\n\tcd $c &&" \
+    "SU2_CFD -t $NProc C.cfg >& cfd.log &&" \
     "awk -v alpha=$alpha -v M=$M -f $TopDir/EWCoeffs.awk cfd.log > Coeffs" \
     >> Makefile
 done
